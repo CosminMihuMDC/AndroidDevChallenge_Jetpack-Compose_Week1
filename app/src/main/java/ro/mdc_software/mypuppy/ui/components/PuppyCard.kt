1dbb20c182c8380f32a7dd004bf4b1e10d80894e
Cosmin Mihu <cosmin.mihu@gmail.com>
2021-02-27 19:14:24 +0200

    Ui work

--- app/src/main/java/ro/mdc_software/mypuppy/ui/components/PuppyCard.kt
+++ app/src/main/java/ro/mdc_software/mypuppy/ui/components/PuppyCard.kt
@@ -2,17 +2,21 @@ package ro.mdc_software.mypuppy.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.chrisbanes.accompanist.coil.CoilImage
+import ro.mdc_software.mypuppy.R
 import ro.mdc_software.mypuppy.data.Puppy
 import ro.mdc_software.mypuppy.data.PuppyGenerator
 import ro.mdc_software.mypuppy.ui.theme.MyPuppyTheme
@@ -33,36 +37,69 @@ fun PuppyCard(puppy: Puppy, modifier: Modifier = Modifier) {
         Column {
             Row {
                 puppy.image?.let {
-                    CoilImage(
-                        modifier = Modifier
-                            .width(100.dp)
-                            .height(80.dp),
-                        data = it,
-                        contentDescription = null,
-                        fadeIn = true,
-                        contentScale = ContentScale.Crop,
-                    )
+                    PuppyImage(it)
                 }
 
                 Column(modifier = Modifier.padding(10.dp)) {
                     Text(
                         text = puppy.name,
-                        style = MaterialTheme.typography.h6
+                        style = MaterialTheme.typography.h6,
+                        maxLines = 1
                     )
                     Text(
                         text = "${puppy.age} years • ${puppy.breed}",
-                        style = MaterialTheme.typography.subtitle1
+                        style = MaterialTheme.typography.subtitle1,
+                        maxLines = 2
                     )
                 }
             }
 
             puppy.description?.let {
                 AnimatedVisibility(visible = descriptionVisible) {
-                    Text(text = it, modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = it,
+                        modifier = Modifier.padding(10.dp),
+                        style = MaterialTheme.typography.caption
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+private fun PuppyImage(imageUrl: String) {
+    CoilImage(
+        modifier = Modifier
+            .width(100.dp)
+            .height(80.dp),
+        data = imageUrl,
+        contentDescription = null,
+        fadeIn = true,
+        contentScale = ContentScale.Crop,
+        loading = {
+            Box(Modifier.fillMaxSize()) {
+                Image(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .align(Alignment.Center),
+                    painter = painterResource(id = R.drawable.ic_dog),
+                    contentDescription = null
+                )
+            }
+        },
+        error = {
+            Box(Modifier.fillMaxSize()) {
+                Image(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .align(Alignment.Center),
+                    painter = painterResource(id = R.drawable.ic_dog),
+                    contentDescription = null
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
